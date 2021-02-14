235a1ba57db201aa6e354a2bcf1bf0f4ddadeaa5
David Vardanyan <davvar40@gmail.com>
2021-02-15 01:29:50 +0400

    role based authentication

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -1,11 +1,31 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
-    const { user } = GqlExecutionContext.create(context).getContext();
+  constructor(private readonly reflector: Reflector){}
 
-    return Boolean(user);
+  canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get<AllowedRoles>(
+      'roles',
+      context.getHandler(),
+    );
+
+    if (!roles) {
+      return true;
+    }
+
+    const { user } = GqlExecutionContext.create(context).getContext();
+    if (!user) {
+      return false;
+    }
+
+    if (roles.includes('ANY')) {
+      return true;
+    }
+
+    return roles.includes(user.role);
   }
 }
